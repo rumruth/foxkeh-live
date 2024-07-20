55342d7b710309099a88d1f5d00a2f82d6048093
rumruth <43695821+rumruth@users.noreply.github.com>
2024-07-20 16:33:02 +0300

    Added SVGs

--- Foxkeh.svelte
+++ Foxkeh.svelte
@@ -1,5 +1,28 @@
 <script>
-  import Main from "./components/Main.svelte";
+  import Head from "./components/Head.svelte";
+  import Body from "./components/Body.svelte";
+  import Tail from "./components/Tail.svelte";
+  import Globe from "./components/Globe.svelte";
 </script>
 
-<Main />
+<div class="temp-wrapper">
+  <div class="foxkeh">
+    <Head />
+    <Body />
+    <Tail />
+    <Globe />
+  </div>
+</div>
+
+<style lang="scss">
+  .temp-wrapper {
+    width: 200px;
+    height: 200px;
+  }
+
+  .foxkeh {
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1 / 1;
+  }
+</style>
---
+++ components/Globe.svelte
@@ -0,0 +1,91 @@
+<script>
+
+</script>
+
+<div class="globe">
+  <svg  version="1.1" id="レイヤー_1" xmlns="&ns_svg;" xmlns:xlink="&ns_xlink;" width="127.76" height="135.013" viewBox="0 0 127.76 135.013" overflow="visible" enable-background="new 0 0 127.76 135.013" xml:space="preserve">
+    <symbol  id="ちたま" viewBox="-63.88 -67.506 127.76 135.013">
+      <g>
+        <defs>
+          <path id="XMLID_1_" d="M63.88,1.083c0-34.359-27.854-62.213-62.212-62.213S-60.544-33.276-60.544,1.083
+            c0,34.358,27.853,62.211,62.212,62.211S63.88,35.441,63.88,1.083z"/>
+        </defs>
+        <radialGradient id="XMLID_9_" cx="-23.6777" cy="31.0361" r="96.8226" gradientUnits="userSpaceOnUse">
+          <stop  offset="0" style="stop-color:#67C5D5"/>
+          <stop  offset="0.1604" style="stop-color:#66C2D3"/>
+          <stop  offset="0.2795" style="stop-color:#62B9CE"/>
+          <stop  offset="0.3852" style="stop-color:#5CA8C6"/>
+          <stop  offset="0.4832" style="stop-color:#5392BA"/>
+          <stop  offset="0.5759" style="stop-color:#4874AA"/>
+          <stop  offset="0.6646" style="stop-color:#3A5097"/>
+          <stop  offset="0.7485" style="stop-color:#2A2781"/>
+          <stop  offset="0.8146" style="stop-color:#1B006D"/>
+          <stop  offset="1" style="stop-color:#596AAD"/>
+        </radialGradient>
+        <use xlink:href="#XMLID_1_"  fill="url(#XMLID_9_)"/>
+        <clipPath id="XMLID_10_">
+          <use xlink:href="#XMLID_1_" />
+        </clipPath>
+        <g clip-path="url(#XMLID_10_)">
+          <radialGradient id="XMLID_11_" cx="-24.4565" cy="39.1611" r="109.0471" gradientUnits="userSpaceOnUse">
+            <stop  offset="0" style="stop-color:#0F80BC"/>
+            <stop  offset="0.2643" style="stop-color:#0B629B"/>
+            <stop  offset="0.8439" style="stop-color:#031747"/>
+            <stop  offset="1" style="stop-color:#00022F"/>
+          </radialGradient>
+          <path fill="url(#XMLID_11_)" d="M-50.608,38.771c9.954-11.524,21.468,3.904,24.201,5.855s10.148-0.391,10.539-3.903
+            c0.39-3.513,1.561-11.319-3.904-12.101c-5.464-0.78-5.464-3.512-9.758-6.635s-5.854-1.953-10.148-1.172s-9.759-0.391-10.93-4.293
+            c-1.171-3.904-3.123-7.807-5.465-7.807c-2.342,0-7.807,1.561-7.807,1.561S-58.025,47.358-50.608,38.771z"/>
+          <radialGradient id="XMLID_12_" cx="-24.4565" cy="39.1621" r="109.0511" gradientUnits="userSpaceOnUse">
+            <stop  offset="0" style="stop-color:#0F80BC"/>
+            <stop  offset="0.2643" style="stop-color:#0B629B"/>
+            <stop  offset="0.8439" style="stop-color:#031747"/>
+            <stop  offset="1" style="stop-color:#00022F"/>
+          </radialGradient>
+          <path fill="url(#XMLID_12_)" d="M-39.679,12.618c1.144,4.576,6.636,8.197,7.417,4.294s1.951-11.319-2.342-8.978
+            C-38.898,10.276-41.24,6.374-39.679,12.618z"/>
+          <radialGradient id="XMLID_13_" cx="-24.4561" cy="39.1621" r="109.0498" gradientUnits="userSpaceOnUse">
+            <stop  offset="0" style="stop-color:#0F80BC"/>
+            <stop  offset="0.2643" style="stop-color:#0B629B"/>
+            <stop  offset="0.8439" style="stop-color:#031747"/>
+            <stop  offset="1" style="stop-color:#00022F"/>
+          </radialGradient>
+          <path fill="url(#XMLID_13_)" d="M-2.597-65.839c-19.909,4.74-29.274,14.833-22.639,23.42c6.635,8.588,8.978,15.224,12.1,19.127
+            s8.531,0.814,10.539-0.391c3.903-2.342,10.149,8.197,10.93-2.342S5.6-37.345,7.161-38.125c6.177-3.089,3.513,6.636,3.513,6.636
+            s2.732,9.368,5.855,13.271s4.294-8.588,4.294-11.32s8.587-5.074,8.587-8.197c0-3.122,4.293-1.951-1.952-9.758
+            s-4.684-11.32-5.464-16.395S5.6-67.79-2.597-65.839z"/>
+          <radialGradient id="XMLID_14_" cx="-24.4136" cy="39.1621" r="109.006" gradientUnits="userSpaceOnUse">
+            <stop  offset="0" style="stop-color:#0F80BC"/>
+            <stop  offset="0.2643" style="stop-color:#0B629B"/>
+            <stop  offset="0.8439" style="stop-color:#031747"/>
+            <stop  offset="1" style="stop-color:#00022F"/>
+          </radialGradient>
+          <path fill="url(#XMLID_14_)" d="M5.209,1.299C2.113,3.708,6.662,5.265,9.503,13.79c1.171,3.513,3.904,4.684,4.294,12.49
+            s3.903,11.71,4.684,15.613S4.039,58.288,7.162,66.485c3.123,8.196,66.861-35.588,51.914-37.082
+            c-27.323-2.732-6.245-16.004-17.174-18.736s2.732-1.172-3.904-5.074c-6.635-3.904-10.149,2.342-13.662-4.294
+            S8.722-1.434,5.209,1.299z"/>
+        </g>
+      </g>
+      <linearGradient id="XMLID_15_" gradientUnits="userSpaceOnUse" x1="0.522" y1="21.1445" x2="0.522" y2="49.4458">
+        <stop  offset="0.0137" style="stop-color:#000000"/>
+        <stop  offset="0.8224" style="stop-color:#C4E0E3"/>
+        <stop  offset="0.8498" style="stop-color:#CDE5E8"/>
+        <stop  offset="0.9443" style="stop-color:#E8F4F7"/>
+        <stop  offset="1" style="stop-color:#F2FAFC"/>
+      </linearGradient>
+      <path opacity="0.5" fill="url(#XMLID_15_)" d="M39.04,35.233c0-13.783-17.249-24.953-38.518-24.953
+        c-21.274,0-38.517,11.17-38.517,24.953c0,13.782,17.243,24.952,38.517,24.952C21.791,60.186,39.04,49.016,39.04,35.233z"/>
+    </symbol>
+    <use xlink:href="#ちたま"  width="127.76" height="135.013" x="-63.88" y="-67.506" transform="matrix(1 0 0 -1 63.8799 67.5068)"/>
+  </svg>
+</div>
+
+<style lang="scss">
+  .globe {
+    position: absolute;
+    z-index: 2;
+    bottom: 1%;
+    left: 4%;
+    width: 60%;
+  }
+</style>
