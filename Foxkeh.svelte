091f8b72fc016f45294f461a145a98e4249a810d
rumruth <43695821+rumruth@users.noreply.github.com>
2024-07-20 19:05:58 +0300

    Cleared the test wrapper

--- Foxkeh.svelte
+++ Foxkeh.svelte
@@ -4,36 +4,26 @@
   import Tail from "./components/Tail.svelte";
 </script>
 
-<div class="temp-wrapper">
-  <div class="foxkeh__wrapper">
-    <div class="foxkeh">
-      <Head />
-      <Body />
-      <Tail />
-    </div>
-  </div>
+<div class="foxkeh">
+  <Head />
+  <Body />
+  <Tail />
 </div>
 
 <style lang="scss">
-  .temp-wrapper {
-    width: 400px;
-    height: 400px;
-    background: blue;
-  }
-
   .foxkeh {
     position: absolute;
     aspect-ratio: 1 / 1;
     width: 50%;
     bottom: 0;
     left: 21%;
+  }
 
-    &__wrapper {
-      box-sizing: border-box;
-      position: relative;
-      bottom: 0;
-      width: 100%;
-      height: 100%;
-    }
+  :global(.foxkeh__wrapper) {
+    box-sizing: border-box;
+    position: relative;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
   }
 </style>
--- foxkeh.js
+++ foxkeh.js
@@ -2,6 +2,8 @@ import Foxkeh from './Foxkeh.svelte';
 
 var div = document.createElement('DIV');
 var script = document.currentScript;
+
+div.classList.add("foxkeh__wrapper");
 script.parentNode.insertBefore(div, script);
 
 const foxkeh = new Foxkeh({
