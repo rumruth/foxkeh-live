a780deff9a5bea7f2e36daad53e5038fe36500f7
rumruth <43695821+rumruth@users.noreply.github.com>
2024-07-21 00:25:47 +0300

    Adding math

--- Foxkeh.svelte
+++ Foxkeh.svelte
@@ -2,10 +2,41 @@
   import Head from "./components/Head.svelte";
   import Body from "./components/Body.svelte";
   import Tail from "./components/Tail.svelte";
+
+  let innerWidth = 0, innerHeight = 0;
+
+  let x_pixels = 0;
+  let y_pixels = 0;
+  /*
+  function percentage(partialValue, totalValue) {
+    return (100 * partialValue) / totalValue;
+  } 
+
+  function mouse_handler(e) {
+    x_percent = Math.ceil(percentage(e.clientX, innerWidth));
+    y_percent = Math.ceil(percentage(e.clientY, innerHeight));
+  }
+  */
+
+  function mouse_handler(e) {
+    x_pixels = Math.ceil(e.clientX);
+    y_pixels = Math.ceil(e.clientY);
+  }
+  /*
+  $: if (x_percent !== 0) {
+    //console.log("Moving X to", x_percent, "%");
+  }
+
+  $: if (y_percent !== 0) {
+    //console.log("Moving Y to", y_percent, "%");
+  }
+  */
 </script>
 
+<svelte:window on:mousemove={mouse_handler} bind:innerWidth bind:innerHeight />
+
 <div class="foxkeh">
-  <Head />
+  <Head bind:x_pixels bind:y_pixels />
   <Body />
   <Tail />
 </div>
